c4fdf93b4f6cf629b300deace1c05f34d518b5b5
fbudin69500 <fbudin@unc.edu>
2015-02-20 11:59:06 -0500

    ENH: Move bootstrap test at the end of script ENH: addition of an option to only display significant LVs. Otherwise script plots first five LVs. BUG: To recompute the original matrix from the SVD decomposition, we were using U*S*V instead of U*S*V'

--- PLS.m
+++ PLS.m
@@ -1,4 +1,4 @@
-function [probaInertia,probaSingularValue,Percent,UOutputnames,VOutputnames,U,S,V,Lx,Ly]=PLS(Data,Groups,GroupNorm,splitIndex,nbPerm,names,ProbaPerVector)
+function [probaInertia,probaSingularValue,Percent,UOutputnames,VOutputnames,U,S,V,Lx,Ly]=PLS(Data,Groups,GroupNorm,splitIndex,nbPerm,names,ProbaPerVector,useSI)
     %Normalizing data
     nbgroups=max(Groups);
     normedData=PLSNormalizeData(Data,GroupNorm);
@@ -19,31 +19,13 @@ function [probaInertia,probaSingularValue,Percent,UOutputnames,VOutputnames,U,S,
       [probaInertia,probaSingularValue,Percent]=PLSPermutations(X,Y,S,nbPerm,Groups);
     end
     %Find singular values that are below a threshold (p<0.05)
-    sI=find(probaSingularValue < 0.05  );
+    if useSI ~= 0
+      sI=find(probaSingularValue < 0.05  );
+    else
+      sI=1:5
+    end
     nbSignificant=min(5,size(sI,2));
     sIPlot=sI(:,1:nbSignificant);
-    %sI(:,5:end)=[];
-    %Boostrap
-    UOutputnames=0;
-    VOutputnames=0;
-    [Uratio,UConfInf,UConfSup,Vratio,VConfInf,VConfSup]=PLSBootStrap(Data,GroupNorm,splitIndex,U,V,S,nbPerm,sIPlot);
-    
-    UOutputnames=cell(size(UInputnames,1),size(sIPlot,2)*nbgroups);
-    VOutputnames=cell(size(VInputnames,1),size(sIPlot,2));
-    for i=1:size(sIPlot,2)
-        for k=1:nbgroups
-            Uindex=1:size(Y,2);
-            Uindex=Uindex+(k-1)*size(Y,2);
-            Unames=PLSStable(Uratio(Uindex,:),UConfInf(Uindex,:),UConfSup(Uindex,:),UInputnames,i);
-            for j =1:size(Unames,1)
-               UOutputnames{j,i*(nbgroups-1)+(k-1)} = Unames{j,1};
-            end
-        end
-        Vnames=PLSStable(Vratio,VConfInf,VConfSup,VInputnames,i);
-        for j =1:size(Vnames,1)
-          VOutputnames{j,i} = Vnames{j,1};
-        end
-    end
     %Latent variables & plots
     Lx=X*V;
 
@@ -99,6 +81,29 @@ function [probaInertia,probaSingularValue,Percent,UOutputnames,VOutputnames,U,S,
         clear Ureshaped;
     end
     hold off
+
+    
+    %Boostrap
+    UOutputnames=0;
+    VOutputnames=0;
+    [Uratio,UConfInf,UConfSup,Vratio,VConfInf,VConfSup]=PLSBootStrap(Data,GroupNorm,splitIndex,U,V,S,nbPerm,sIPlot);
+    
+    UOutputnames=cell(size(UInputnames,1),size(sIPlot,2)*nbgroups);
+    VOutputnames=cell(size(VInputnames,1),size(sIPlot,2));
+    for i=1:size(sIPlot,2)
+        for k=1:nbgroups
+            Uindex=1:size(Y,2);
+            Uindex=Uindex+(k-1)*size(Y,2);
+            Unames=PLSStable(Uratio(Uindex,:),UConfInf(Uindex,:),UConfSup(Uindex,:),UInputnames,i);
+            for j =1:size(Unames,1)
+               UOutputnames{j,i*(nbgroups-1)+(k-1)} = Unames{j,1};
+            end
+        end
+        Vnames=PLSStable(Vratio,VConfInf,VConfSup,VInputnames,i);
+        for j =1:size(Vnames,1)
+          VOutputnames{j,i} = Vnames{j,1};
+        end
+    end
     
     %Permutations Lx and Ly
     %for j=1:nbPerm
